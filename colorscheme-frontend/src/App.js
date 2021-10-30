163df5fde6a7ae6d0c3b6a6f5171569976a915da
thijn <thijnvandenbergh@gmail.com>
2021-10-30 14:55:25 +0200

    added simple front end

---
+++ colorscheme-frontend/src/App.js
@@ -0,0 +1,54 @@
+import {useState} from 'react'
+import './App.scss';
+
+function App() {
+  const [preview,setPreview] = useState()
+  const [theme, setTheme] = useState([])
+  function handleFileInput(e) {
+    console.log(e.target.files[0])
+    if(e.target.files[0]) setPreview(URL.createObjectURL(e.target.files[0]));
+  }
+
+  async function getScheme() {
+    const formData = new FormData();
+    formData.append('file', document.getElementById("fileinput").files[0]);
+    const options = {
+      method: 'POST',
+      body: formData,
+      // If you add this, upload won't work
+      // headers: {
+      //   'Content-Type': 'multipart/form-data',
+      // }
+    };
+    
+    document.getElementById("loading").style.display = "flex";
+    let response = await fetch('http://localhost:5000/upload', options)
+    if(response.ok) {
+      let json = await response.json()
+      setTheme(json)
+    } else {
+      console.error("something went wrong")
+    }
+    document.getElementById("loading").style.display = "none";
+  }
+
+  return (
+    <div className="app">
+      <div id="loading"><img src="https://c.tenor.com/5o2p0tH5LFQAAAAi/hug.gif"/></div>
+      <div className="block upload">
+        <input id="fileinput" onInput={handleFileInput} type="file"/>
+        <button onClick={getScheme}>Get Scheme</button>
+      </div>
+      <div className="block preview">
+        <img src={preview}/>
+      </div>
+      <div className="block theme">
+        {
+          theme.map(hex => <span style={{backgroundColor:hex}}>{hex}</span>)
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
